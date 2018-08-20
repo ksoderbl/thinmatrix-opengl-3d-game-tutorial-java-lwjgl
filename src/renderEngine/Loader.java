dea9081fcd20222b0d37734e1a4bf601b57f2535
Kristian Söderblom <28917898+ksoderbl@users.noreply.github.com>
2018-08-20 20:29:37 +0300

    OpenGL 3D Game Tutorial 20: Mipmapping completed

--- src/renderEngine/Loader.java
+++ src/renderEngine/Loader.java
@@ -2,10 +2,7 @@ package renderEngine;
 
 import models.RawModel;
 import org.lwjgl.BufferUtils;
-import org.lwjgl.opengl.GL11;
-import org.lwjgl.opengl.GL15;
-import org.lwjgl.opengl.GL20;
-import org.lwjgl.opengl.GL30;
+import org.lwjgl.opengl.*;
 import org.newdawn.slick.opengl.Texture;
 import org.newdawn.slick.opengl.TextureLoader;
 
@@ -45,6 +42,9 @@ public class Loader {
         Texture texture = null;
         try {
             texture = TextureLoader.getTexture("PNG", new FileInputStream("res/" + fileName + ".png"));
+            GL30.glGenerateMipmap(GL11.GL_TEXTURE_2D);
+            GL11.glTexParameteri(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_MIN_FILTER, GL11.GL_LINEAR_MIPMAP_LINEAR);
+            GL11.glTexParameterf(GL11.GL_TEXTURE_2D, GL14.GL_TEXTURE_LOD_BIAS, -1.4f);
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         } catch (IOException e) {
