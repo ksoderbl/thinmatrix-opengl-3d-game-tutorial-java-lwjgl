e88678d2bd84c6f4b1ca0a716a89208d9523df9d
Kristian Söderblom <28917898+ksoderbl@users.noreply.github.com>
2019-10-18 22:49:42 +0300

    Add MainGameLoop12 etc. to run Tutorial 12

---
+++ src/shaders/StaticShader12.java
@@ -0,0 +1,66 @@
+package shaders;
+
+import org.lwjgl.util.vector.Matrix4f;
+
+import entities.Camera12;
+import entities.Light;
+import toolbox.Maths;
+
+public class StaticShader12 extends ShaderProgram {
+
+    private static final String VERTEX_FILE = "src/shaders/vertexShader12.glsl";
+    private static final String FRAGMENT_FILE = "src/shaders/fragmentShader12.glsl";
+    
+    private int location_transformationMatrix;
+    private int location_projectionMatrix;
+    private int location_viewMatrix;
+    private int location_lightPosition;
+    private int location_lightColor;
+    private int location_shineDamper;
+    private int location_reflectivity;
+
+    public StaticShader12() {
+        super(VERTEX_FILE, FRAGMENT_FILE);
+    }
+
+    @Override
+    protected void bindAttributes() {
+        super.bindAttribute(0, "position");
+        super.bindAttribute(1, "textureCoords");
+        super.bindAttribute(2, "normal");
+    }
+
+	@Override
+	protected void getAllUniformLocations() {
+		location_transformationMatrix = super.getUniformLocation("transformationMatrix");
+		location_projectionMatrix = super.getUniformLocation("projectionMatrix");
+		location_viewMatrix = super.getUniformLocation("viewMatrix");
+		location_lightPosition = super.getUniformLocation("lightPosition");
+		location_lightColor = super.getUniformLocation("lightColor");
+		location_shineDamper = super.getUniformLocation("shineDamper");
+		location_reflectivity = super.getUniformLocation("reflectivity");
+	}
+	
+	public void loadShineVariables(float damper, float reflectivity) {
+		super.loadFloat(location_shineDamper, damper);
+		super.loadFloat(location_reflectivity, reflectivity);
+	}
+		
+	public void loadTransformationMatrix(Matrix4f matrix) {
+		super.loadMatrix(location_transformationMatrix, matrix);
+	}
+	
+	public void loadLight(Light light) {
+		super.loadVector(location_lightPosition, light.getPosition());
+		super.loadVector(location_lightColor, light.getColor());
+	}
+	
+    public void loadViewMatrix(Camera12 camera) {
+        Matrix4f viewMatrix = Maths.createViewMatrix(camera.getPosition(), camera.getPitch(), camera.getYaw(), camera.getRoll());
+        super.loadMatrix(location_viewMatrix, viewMatrix);
+    }
+
+	public void loadProjectionMatrix(Matrix4f projection) {
+		super.loadMatrix(location_projectionMatrix, projection);
+	}
+}
