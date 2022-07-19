1eeb00cae1019e041148943cc7225d874e78cf77
Kristian Söderblom <28917898+ksoderbl@users.noreply.github.com>
2022-07-20 00:24:56 +0300

    Tutorial 5 now works on lwjgl3.

--- src/com/example/renderEngine/DisplayManager.java
+++ src/com/example/renderEngine/DisplayManager.java
@@ -2,12 +2,16 @@ package com.example.renderEngine;
 
 import static org.lwjgl.glfw.GLFW.*;
 import static org.lwjgl.opengl.GL.*;
+import static org.lwjgl.opengl.GL11.*;
 import static org.lwjgl.system.MemoryUtil.*;
 
 public class DisplayManager {
-    private static final int CREATE_WIDTH = 800;
-    private static final int CREATE_HEIGHT = 600;
+    private static final int CREATE_WIDTH = 1280;
+    private static final int CREATE_HEIGHT = 720;
+    // private static final int FPS_CAP = 120;
     private static long window = -1L;
+
+    // private static boolean vsync = true; // TODO
 
     public static void createDisplay(String title) {
     	createDisplay(title, CREATE_WIDTH, CREATE_HEIGHT);
@@ -15,10 +19,17 @@ public class DisplayManager {
     
     public static void createDisplay(String title, int width, int height) {
         glfwInit();
+        glfwDefaultWindowHints();
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
+        glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GLFW_TRUE);
+        glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+        // glfwWindowHint(GLFW_VISIBLE, GLFW_FALSE);
         glfwWindowHint(GLFW_RESIZABLE, GLFW_TRUE);
         window = glfwCreateWindow(width, height, title, NULL, NULL);
         glfwMakeContextCurrent(window);
         createCapabilities();
+        glViewport(0,0, width, height);
     }
 
     public static boolean isCloseRequested() {
---
+++ src/com/example/renderEngine/Renderer05.java
@@ -0,0 +1,24 @@
+package com.example.renderEngine;
+
+import org.lwjgl.opengl.GL11;
+import org.lwjgl.opengl.GL20;
+import org.lwjgl.opengl.GL30;
+
+import com.example.models.RawModel;
+
+public class Renderer05 {
+
+    public void prepare() {
+        GL11.glClearColor(0, 0, 0.3f, 1);
+        GL11.glClear(GL11.GL_COLOR_BUFFER_BIT);
+    }
+	
+    public void render(RawModel model) {
+    	GL30.glBindVertexArray(model.getVaoID());
+    	GL20.glEnableVertexAttribArray(0);
+    	// OpenGL 3D Game Tutorial 3: Rendering with Index Buffers
+    	GL11.glDrawElements(GL11.GL_TRIANGLES, model.getVertexCount(), GL11.GL_UNSIGNED_INT, 0);
+    	GL20.glDisableVertexAttribArray(0);
+    	GL30.glBindVertexArray(0);
+    }
+}
