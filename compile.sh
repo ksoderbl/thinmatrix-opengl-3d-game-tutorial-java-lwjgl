95d025e7183bdefa8f40a5117724cf8c8baecc77
Kristian Söderblom <28917898+ksoderbl@users.noreply.github.com>
2022-07-29 23:29:33 +0300

    Remove old compile and run scripts.

--- compile.sh
+++
@@ -1,1 +0,0 @@
-javac -cp .:res:lib/jars/lwjgl.jar:lib/jars/lwjgl_util.jar:lib/jars/slick-util.jar:lib/jars/PNGDecoder.jar   src/*/*.java
--- run.sh
+++
@@ -1,1 +0,0 @@
-java -cp .:res:lib/jars/lwjgl.jar:lib/jars/lwjgl_util.jar:lib/jars/slick-util.jar:lib/jars/PNGDecoder.jar:src: -Djava.library.path=lib/natives/  engineTester/MainGameLoop35
