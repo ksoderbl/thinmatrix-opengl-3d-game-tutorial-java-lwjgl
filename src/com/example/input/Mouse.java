679f87594a5d0733a0bcd474472ee733a04c41e5
Kristian Söderblom <28917898+ksoderbl@users.noreply.github.com>
2022-08-04 14:49:02 +0300

    Mouse: Use a HashMap to remember which buttons are down.

--- src/com/example/input/Mouse.java
+++ src/com/example/input/Mouse.java
@@ -1,14 +1,36 @@
 package com.example.input;
 
+import java.util.HashMap;
+
 import static org.lwjgl.glfw.GLFW.*;
 
 // https://legacy.lwjgl.org/javadoc/org/lwjgl/input/Mouse.html
 
 public class Mouse {
 
+    private static HashMap<Integer, String> buttonsDown = new HashMap<Integer, String>();
+
     // Callback method used with Java 8 method references.
     public static void mouseCallback(long window, int button, int action, int mods) {
+
+
+
         System.out.println("mouseCallback: button, action, mods: " + button + ", " + action + ", " + mods);
+
+        switch (button) {
+            case GLFW_MOUSE_BUTTON_LEFT: System.out.println("left"); break;
+            case GLFW_MOUSE_BUTTON_MIDDLE: System.out.println("middle"); break;
+            case GLFW_MOUSE_BUTTON_RIGHT: System.out.println("right"); break;
+            default: break;
+        }
+
+
+        if (action == GLFW_PRESS) {
+            buttonsDown.put(button, "down");
+        }
+        else if (action == GLFW_RELEASE) {
+            buttonsDown.remove(button);
+        }
     }
 
     public static void setWindow(long window) {
@@ -21,8 +43,7 @@ public class Mouse {
     // Returns:
     //     true if the specified button is down
     public static boolean isButtonDown(int button) {
-        // TODO
-        return false;
+        return buttonsDown.containsKey(button);
     }
 
     // Retrieves the absolute position. It will be clamped to 0...width-1.
